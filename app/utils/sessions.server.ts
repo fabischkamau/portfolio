4eca45294636b7d64233f7530b8beef6677ab1d6
fabischkamau <126986727+fabischkamau@users.noreply.github.com>
2024-04-20 13:49:16 +0300

    sessionstorage

--- app/utils/sessions.server.ts
+++ app/utils/sessions.server.ts
@@ -34,9 +34,7 @@ const { getSession, commitSession, destroySession } =
       name: "__session",
 
       // all of these are optional
-      ...(isProduction
-        ? { domain: "https://fabischkamau.vercel.app", secure: true }
-        : {}),
+      ...(isProduction ? { domain: webUrl, secure: true } : {}),
       // Expires can also be set (although maxAge overrides it when used in combination).
       // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
       //
